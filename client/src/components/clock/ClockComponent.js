dbdce1b4d9f53c98f6f3175d3756adf444383834
Avijeet Pandey <avijeetpandey25@gmail.com>
2020-10-23 12:21:04 +0530

    added resizable component one

--- client/src/components/clock/ClockComponent.js
+++ client/src/components/clock/ClockComponent.js
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from "react";
-import 'react-clock/dist/Clock.css';
+import { Resizable } from "re-resizable";
+import "react-clock/dist/Clock.css";
 import Clock from "react-clock";
 
 const ClockComponent = () => {
+  const style = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    border: "solid 1px #ddd",
+    background: "#f0f0f0",
+  };
+
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
@@ -13,7 +22,19 @@ const ClockComponent = () => {
     };
   }, []);
 
-  return <Clock value={time} />;
+  return (
+    <div>
+      <Resizable
+        style={style}
+        defaultSize={{
+          width: 320,
+          height: "80vh",
+        }}
+      >
+        <Clock value={time} />
+      </Resizable>
+    </div>
+  );
 };
 
 export default ClockComponent;
